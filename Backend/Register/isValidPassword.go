1f26533545d0f710b3980ce74b0b1d187e0bbc43
alisa <alooy20066mohd@gmail.com>
2025-09-14 17:59:39 +0300

    removed password constrains

--- Backend/Register/isValidPassword.go
+++ Backend/Register/isValidPassword.go
@@ -2,6 +2,7 @@ package register
 
 import (
 	"errors"
+	"strings"
 )
 
 func isValidPassword(password string) (bool, error) {
@@ -11,6 +12,8 @@ func isValidPassword(password string) (bool, error) {
 	if len(password) > 24 {
 		return false, errors.New("password must not exceed 24 characters")
 	} // Password too long
-
+	if strings.Contains(password, " ") {
+		return false, errors.New("password must not contain spaces")
+	}
 	return true, nil // Password is valid
 }
